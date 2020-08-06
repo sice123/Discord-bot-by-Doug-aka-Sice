44996dd8bf93b6dbf5fdef21e45d5630ea596d35
PboyDoug <67115503+sice123@users.noreply.github.com>
2020-08-06 23:11:56 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,28 +7,35 @@ module.exports = {
         execute(message, args){
              const embed = new Discord.MessageEmbed();
              embed.setTitle('list of commands');
-             embed.setColor('WHITE')
+             embed.setColor('BLACK')
              
              embed.addField('# Modaration' ,[
                  '**`$kick`**-Kick a member.',
                  '**`$Ban`**-Ban a member.',
                  '**`$Mute`**-Mute a member.',
                  '**`$Clear`**-Clears from 2-99 messages.'
-
-                 
              ])
             embed.addField('# Miscellaneous',[
                 '**`$Help`**-Show a list of commands',
                 '**`$Ping`**-Pong!',
                 '**`$ServerInfo`**-Shows server information',
-                '**`$Userinfo`**-Shows user information'
+                '**`$Userinfo`**-Shows user information',
+                '**`$Discord`**-Discord server invite',
             ])                 
              embed.addField('# Embeds',[
-                 '**`$Embedron`**-<3'
+                 '**`$Embedron`**-<3',
+                 '**`$Embedalmog`**-:-)',
              ])
              embed.addField('# Who is the killer',[
                  '**`$Kill`**-Choose who you want to kill',
+                 '**`$Protect`**-Choose who to protect',
+                 '**`$1-11`**-Secret',
 
+             ])
+             embed.addField('# Music',[
+                 '**`$Play`**-Play / Queue a song use a valid youtube url!',
+                 '**`$Stop`**-The bot will disconnect from the channel',
+                 '**`$Skip`**-Skip the current song'
              ])
 
 
